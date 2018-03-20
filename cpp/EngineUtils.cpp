df92d2e4cff6df2097bb38f2793515406e54e417
KyoKeun Park <kyokeun.park@mail.utoronto.ca>
2018-03-19 20:47:50 -0400

    EngineUtils ported

---
+++ cpp/EngineUtils.cpp
@@ -0,0 +1,38 @@
+//
+//  EngineUtils.cpp
+//  Boltz
+//
+//  Created by KyoKeun Park on 2018-03-19.
+//  Copyright © 2018 KyoKeun Park. All rights reserved.
+//
+
+#include "EngineUtils.hpp"
+
+int random(int min, int max)
+{
+    std::default_random_engine gen (std::chrono::system_clock::now().time_since_epoch().count());
+    std::uniform_real_distribution<double> dist(min, max);
+    return (int) dist(gen);
+}
+int generateNonZero(int absoluteRange)
+{
+    int num = random(-absoluteRange, absoluteRange);
+    if (num == 0)
+        num++;
+    return num;
+}
+
+int generatePositiveRandom(int min, int max)
+{
+    return random(min, max);
+}
+
+int generatePositiveRandom(int max)
+{
+    return random(1, max);
+}
+
+int generatePosNegRandom(int range)
+{
+    return random(-range, range);
+}
---
+++ cpp/EngineUtils.hpp
@@ -0,0 +1,19 @@
+//
+//  EngineUtils.hpp
+//  Boltz
+//
+//  Created by KyoKeun Park on 2018-03-19.
+//  Copyright © 2018 KyoKeun Park. All rights reserved.
+//
+
+#ifndef EngineUtils_hpp
+#define EngineUtils_hpp
+
+#include <random>
+
+int generateNonZero(int absoluteRange);
+int generatePositiveRandom(int min, int max);
+int generatePositiveRandom(int max);
+int generatePosNegRandom(int range);
+
+#endif /* EngineUtils_hpp */
