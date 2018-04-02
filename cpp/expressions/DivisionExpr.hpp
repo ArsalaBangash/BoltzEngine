c92f51a27ecc40a2a437c4b40669a4c32421b311
KyoKeun Park <kyokeun.park@mail.utoronto.ca>
2018-04-02 17:17:09 -0400

    started working on division expression

---
+++ cpp/expressions/DivisionExpr.hpp
@@ -0,0 +1,31 @@
+//
+//  DivisionExpr.hpp
+//  Boltz
+//
+//  Created by KyoKeun Park on 2018-04-02.
+//  Copyright © 2018 KyoKeun Park. All rights reserved.
+//
+
+#ifndef DivisionExpr_hpp
+#define DivisionExpr_hpp
+
+#include "MathExpr.hpp"
+#include "ExprToken.hpp"
+
+class DivisionExpr: public MathExpr
+{
+    void setMax(Level);
+    void addZeroBoundTokens(SubExprLocation);
+    void noSubExpressions();
+    void twoSubExpressions();
+    void oneSubExpressions(SubExprLocation);
+    std::vector<ExprToken> produceExpression(SubExprLocation);
+    
+public:
+    DivisionExpr(MathOperation mathOperation) : MathExpr(mathOperation) {}
+    
+private:
+    int generateDivisor(int);
+};
+
+#endif /* DivisionExpr_hpp */
