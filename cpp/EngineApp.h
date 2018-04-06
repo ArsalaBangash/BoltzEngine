0aa3cc4abc137e7173ef8d6332d50fb374544423
KyoKeun Park <kyokeun.park@mail.utoronto.ca>
2018-04-06 00:21:31 -0400

    added function signatures for EngineApp.h

--- cpp/EngineApp.h
+++ cpp/EngineApp.h
@@ -6,10 +6,14 @@
 #define PROJECT_ENGINEAPP_H
 
 #include <string>
+#include "Enums.h"
 
 struct ChallengeReps {
     std::string infixRepr;
     std::string latexRepr;
 };
+
+bool checkAnswer(const std::string&, const std::string&);
+ChallengeReps genExpression(std::string, Level);
 
 #endif //PROJECT_ENGINEAPP_H
