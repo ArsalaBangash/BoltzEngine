795dc58aa3f23825579e753be1b7ba0287661678
Arsala Grey <arsalagrey@Arsalas-MacBook-Pro-2.local>
2019-05-24 07:24:48 -0700

    Hey

--- kotlin/src/main/java/com/arsalabangash/boltzengine/engine/EngineTest.kt
+++ kotlin/src/main/java/com/arsalabangash/boltzengine/engine/EngineTest.kt
@@ -27,9 +27,9 @@ fun main(args: Array<String>) {
     (1..20).forEach {
         acc += testTime(numExpressions)
     }
-    val a = 10
+    val a = 1000
     println("Generated $numExpressions expressions in " + (acc / 20) + " milliseconds")
-    val c = 10000
+    val b = 3
 
 }
 
